3baebb7b1993ae25c24a93749713761f7f780daa
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-10 19:04:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
       const banner = document.getElementById('banner');
       const images = banner.querySelectorAll('.banner-container img');
       const dotsContainer = banner.querySelector('.banner-dots');
+      let currentSlide = 0;
